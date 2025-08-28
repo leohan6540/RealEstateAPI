19c35677c58defd1ed05c9ef130ca65c6dbe53b9
leohan6540 <leohan6540@gmail.com>
2025-08-28 09:57:06 +0900

    feat: searbar 완성

--- src/components/searchSection/SearchBar.jsx
+++ src/components/searchSection/SearchBar.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { inputSize, inputStyle } from "./SearchbarToken.js";
 
-const SearchBar = ({ placeholder }) => {
-  return <input placeholder={placeholder} />;
+const SearchBar = ({
+  placeholder,
+  searchbarSize = "searchbar500",
+  searchbarStyle = "searchbar500",
+  className,
+  //value,
+  ...props
+}) => {
+  const sizeClass = inputSize[searchbarSize] || "";
+  const styleClass = inputStyle[searchbarStyle] || "";
+
+  return (
+    <div>
+      <img
+        className="relative top-8"
+        src="./searchIcon.svg"
+        alt="검색 아이콘"
+      />
+      <input
+        placeholder={placeholder}
+        className={`${className} ${sizeClass} ${styleClass}`}
+        {...props}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
